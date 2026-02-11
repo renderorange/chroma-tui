978fe0b4d7cb59ebb25725f73150a29b948b812b
Blaine Motsinger <blaine@renderorange.com>
2026-02-10 22:48:44 -0600

    Wire up OSC state sync between TUI and SuperCollider

--- main.go
+++ main.go
@@ -16,20 +16,35 @@ func main() {
 	listenPort := flag.Int("listen", 9000, "Port to listen for state updates")
 	flag.Parse()
 
-	// Create OSC client
+	// Create OSC client and server
 	client := osc.NewClient(*scHost, *scPort)
+	server := osc.NewServer(*listenPort)
 
-	// Create and run TUI
+	// Start OSC server in background
+	go func() {
+		if err := server.Start(); err != nil {
+			fmt.Fprintf(os.Stderr, "OSC server error: %v\n", err)
+		}
+	}()
+
+	// Create TUI model
 	model := tui.NewModel(client)
+
+	// Create program
+	p := tea.NewProgram(model, tea.WithAltScreen())
+
+	// Forward state updates to TUI
+	go func() {
+		for state := range server.StateChan() {
+			p.Send(tui.StateMsg(state))
+		}
+	}()
 
 	// Request initial state
 	client.SendSync()
 
-	p := tea.NewProgram(model, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
-
-	_ = listenPort // Will be used in Task 8
 }
